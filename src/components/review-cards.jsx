55d6f431dc3df9891cabb3947db14bb0d4b4890f
Keith Tachibana <Keith.Tachibana@gmail.com>
2020-03-05 23:51:52 -0800

    React Flash Cards PT: Review Progress Bar | Keith Tachibana

--- src/components/review-cards.jsx
+++ src/components/review-cards.jsx
@@ -1,16 +1,20 @@
 import React, { Component } from 'react';
 
+import ProgressBar from './progress-bar';
+
 class ReviewCards extends Component {
   constructor(props) {
     super(props);
     this.state = {
       front: true,
-      imageIndex: 0
+      imageIndex: 0,
+      progress: 0
     };
 
     this.nextCard = this.nextCard.bind(this);
     this.previousCard = this.previousCard.bind(this);
     this.flipCard = this.flipCard.bind(this);
+    this.setProgress = this.setProgress.bind(this);
   }
 
   componentDidMount() {
@@ -24,7 +28,7 @@ class ReviewCards extends Component {
       return (this.state.imageIndex === cards.length - 1)
         ? { imageIndex: 0 }
         : { imageIndex: this.state.imageIndex + 1 }
-    }, setActiveCard(this.state.imageIndex));
+    }, () => setActiveCard(this.state.imageIndex), this.setProgress());
   }
 
   previousCard() {
@@ -33,7 +37,7 @@ class ReviewCards extends Component {
       return (this.state.imageIndex === 0)
         ? { imageIndex: cards.length - 1 }
         : { imageIndex: this.state.imageIndex - 1 }
-    }, setActiveCard(this.state.imageIndex));
+    }, () => setActiveCard(this.state.imageIndex), this.setProgress());
   }
 
   showCard() {
@@ -44,6 +48,7 @@ class ReviewCards extends Component {
       return (
         this.state.front
           ? <React.Fragment>
+              <ProgressBar progress={this.state.progress}/>
               <div className="carousel bg-dark">
                 <i className="previous fas fa-chevron-left fa-5x" onClick={this.previousCard}></i>
                 <div className="card-wrapper" onClick={this.flipCard}>
@@ -68,6 +73,15 @@ class ReviewCards extends Component {
   flipCard() {
     this.setState({
       front: !this.state.front
+    });
+  }
+
+  setProgress() {
+    const { cards } = this.props;
+    const cardsLength = cards.length;
+    const progress = (this.state.imageIndex + 1) / cardsLength;
+    this.setState({
+      progress
     });
   }
 
